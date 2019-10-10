7426ba7ed66bbe88fc35b72671f8386bfc620516
Omoyi <urakhel7@gmail.com>
2019-10-10 15:24:02 +0200

    EXERCISE A LOT OF STUFF

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -1,34 +1,39 @@
 package com.example.myrestaurants;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
-
+//    public static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        ButterKnife.bind(this);
+//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+//        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+
                 String location = mLocationEditText.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-//                startActivity(intent);
-                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+                intent.putExtra("Rachel", location);
+                startActivity(intent);
+
             }
 
         });
